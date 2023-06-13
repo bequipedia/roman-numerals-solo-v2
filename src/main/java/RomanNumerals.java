8e3f46c9662616a32e9b6a993596b43e7d271302
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-13 12:24:16 +0200

    pass failing test

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -2,10 +2,13 @@ public class RomanNumerals {
 
     public static final String I = "I";
     public static final String V = "V";
+    public static final String X = "X";
 
     public String convert(int inputNumber) {
         String output = "";
-
+        if(inputNumber == 10) {
+            output = X;
+        }
         if(inputNumber == 5) {
             output = V;
         }
